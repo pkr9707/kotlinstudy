89fbb92feddccffc4fa3890fa431b3aec862b52d
pkr9707 <31675670+pkr9707@users.noreply.github.com>
2020-02-19 00:01:14 +0900

    upload

---
+++ jun/no.1008.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main(){
+    val sc:Scanner= Scanner(System.`in`)
+    val a=sc.nextInt()
+    val b=sc.nextInt()
+
+    val c : Double = a.toDouble()/b.toDouble()
+
+    print(c)
+
+
+}
