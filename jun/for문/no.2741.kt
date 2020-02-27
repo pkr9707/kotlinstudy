8470280c9c475fb3709f4b9b635b52f41db61d85
pkr9707 <31675670+pkr9707@users.noreply.github.com>
2020-02-28 02:30:12 +0900

    update

---
+++ jun/for문/no.2741.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main(){
+    val sc : Scanner = Scanner(System.`in`)
+    var a = sc.nextInt()
+    var b =1
+    for(i in 1..a){
+        println(b)
+        b+=1
+    }
+}
